46b594e4c042962878c5a0a3f4afa00a6353affd
Tomasz Spodzieja <tomsp77799@gmail.com>
2020-11-09 19:29:40 +0100

    Superuser created and some models improves done

--- main_system/admin.py
+++ main_system/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Location, Contact, Local, Room, Catering, OtherOffer
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Contact)
+admin.site.register(Local)
+admin.site.register(Room)
+admin.site.register(Catering)
+admin.site.register(OtherOffer)
--- main_system/models.py
+++ main_system/models.py
@@ -5,18 +5,27 @@ class Location(models.Model):
     voivodeship = models.CharField(max_length=100)
     district = models.CharField(max_length=100)
     town = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    local_number = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.town + ", powiat " + self.district + ", województwo " + self.voivodeship
+        return self.street + " " + self.local_number + " " + self.town + ", powiat " + self.district +\
+               ", województwo " + self.voivodeship
 
 
 class Contact(models.Model):
     phone_number = models.CharField(max_length=20)
-    website = models.URLField(max_length=100, null=True)
-    email = models.EmailField(max_length=100, null=True)
+    website = models.URLField(max_length=100, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return self.phone_number + " " + self.website + " " + self.email
+        string_website = self.website
+        string_email = self.email
+        if string_website is None:
+            string_website = ""
+        if string_email is None:
+            string_email = ""
+        return self.phone_number + " " + string_website + " " + string_email
 
 
 class Catering(models.Model):
@@ -25,7 +34,7 @@ class Catering(models.Model):
     name = models.CharField(max_length=100)
     with_local_only = models.BooleanField(default=False)
     min_cost_per_person = models.DecimalField(max_digits=10, decimal_places=2)
-    basic_offer = models.CharField(max_length=1000, null=True)
+    basic_offer = models.CharField(max_length=1000, null=True, blank=True)
     added = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -36,7 +45,7 @@ class Catering(models.Model):
 class Local(models.Model):
     location = models.OneToOneField(Location, on_delete=models.CASCADE)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
-    catering = models.OneToOneField(Catering, on_delete=models.CASCADE, null=True)
+    catering = models.OneToOneField(Catering, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     added = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
@@ -49,11 +58,11 @@ class Room(models.Model):
     local = models.ForeignKey(Local, on_delete=models.CASCADE)
     max_people = models.IntegerField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    type_of_parquet = models.CharField(max_length=100, null=True)
-    air_conditioned = models.BooleanField(null=True)
+    type_of_parquet = models.CharField(max_length=100, null=True, blank=True)
+    air_conditioned = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
-        return self.local + " - " + self.max_people + " osób"
+        return self.local.__str__() + " - " + str(self.max_people) + " osób"
 
 
 class OtherOffer(models.Model):
@@ -61,7 +70,7 @@ class OtherOffer(models.Model):
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     min_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    basic_offer = models.CharField(max_length=1000, null=True)
+    basic_offer = models.CharField(max_length=1000, null=True, blank=True)
     added = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
