35f7b76f59c57a10cc6649e66ada73cdb6b76801
Tomasz Spodzieja <tomsp77799@gmail.com>
2020-11-09 13:07:33 +0100

    main_system app added

--- special_events_organizer/settings.py
+++ special_events_organizer/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_system.apps.MainSystemConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
