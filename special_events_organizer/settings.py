086d664b996e9d5c7d13c0df172bb0135f6deed9
Tomasz Spodzieja <tomsp77799@gmail.com>
2020-11-21 17:57:36 +0100

    Password changing works

--- special_events_organizer/settings.py
+++ special_events_organizer/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = ''
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
