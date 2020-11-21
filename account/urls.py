91a1a16ae3446fb52afd06f686e0cffe47c45f1e
Tomasz Spodzieja <tomsp77799@gmail.com>
2020-11-21 20:20:31 +0100

    User registration added

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
+from . import views
 
 
 app_name = 'account'
@@ -18,4 +19,5 @@ urlpatterns = [
         success_url=reverse_lazy('account:password_reset_complete')
     ), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
 ]
