fa6f8fe787a9173f1fb400ae8ec4cbe53267d931
Tomasz Spodzieja <tomsp77799@gmail.com>
2020-12-01 21:13:27 +0100

    Database changed to posgreSQL

--- special_events_organizer/settings.py
+++ special_events_organizer/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'special_events_organizer.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
